81749c31ae188d1907d038ae2666d1eafda89d36
ARYAN KAPOOR <aryanalokkapoor@gmail.com>
2026-06-18 03:04:42 +0530

    added new groq model

--- tav.py
+++ tav.py
@@ -2,8 +2,11 @@ from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.prebuilt import ToolNode
+from dotenv import load_dotenv
 
-llm = ChatGroq(model="llama-3.1-8b-instant")
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
 tools = [TavilySearch(max_results=3)]
 llm_with_tools = llm.bind_tools(tools)
 
@@ -34,5 +37,10 @@ graph.add_edge("search", "llm")  # after search, go back to LLM
 
 agent = graph.compile()
 
-result = agent.invoke({"messages": [{"role": "user", "content": "Who is Messi?"}]})
-print(result["messages"][-1].content)
+result = agent.invoke({"messages": [{"role": "user", "content": "What is latest news in AI World"}]})
+answer = result["messages"][-1].content
+
+with open("output.txt", "w") as f:
+    f.write(answer)
+
+print("Saved to output.txt")
